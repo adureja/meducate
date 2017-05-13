2d09ad242e9b8ca0d8e45c391d9f51e83820600c
Avik Dureja <adurejabackup@gmail.com>
2017-05-12 21:40:04 -0400

    modified port for heroku

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def show_papers(search):
     return names, links
 
 
-
-
-app.run()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
